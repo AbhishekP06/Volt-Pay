df5328e1a1d3740b30e8b41d922c355f01d2db95
AbhishekP06 <webeyes02@gmail.com>
2025-08-29 00:17:03 +0530

    Stop tracking dist folder

--- backend/dist/middlewares/auth.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-export interface AuthRequest extends Request {
-    user?: {
-        id: number;
-        email: string;
-    };
-}
-export declare function authMiddleware(req: AuthRequest, res: Response, next: NextFunction): Response<any, Record<string, any>> | undefined;
-//# sourceMappingURL=auth.d.ts.map
